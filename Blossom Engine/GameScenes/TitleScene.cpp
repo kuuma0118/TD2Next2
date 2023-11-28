da816553609cebd43c1479b6d5baa1a2d245498a
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-11-28 16:48:22 +0900

    カメラ途中。全然うごかねー

--- Blossom Engine/GameScenes/TitleScene.cpp
+++ Blossom Engine/GameScenes/TitleScene.cpp
@@ -10,22 +10,36 @@ void TitleScene::Initialize() {
 	block_ = new Block();
 	block_->Initialize();
 	block_->textureNum = BLOCK;
+	upsidetank_ = new UpsideTank();
+	upsidetank_->Initialize();
+	upsidetank_->textureNum = UPSIDETANK;
+	downsidetank_ = new DownsideTank();
+	downsidetank_->Initialize();
+	downsidetank_->textureNum = DOWNSIDETANK;
 	pos_ = { 0,0,30 };
-
+	debugcamera_ = new DebugCamera();
 }
 
 void TitleScene::Update() {
-	if(input_->TriggerKey(DIK_SPACE)){
+	if (input_->PressKey(DIK_W)) {
+		debugcamera_->translation_.z += 3.00f;
+	}
+	if (input_->TriggerKey(DIK_SPACE)) {
 		sceneNum = GAME_SCENE;
 	}
 }
 
 void TitleScene::Draw() {
 	//block_->Draw();
+	upsidetank_->Draw();
+	downsidetank_->Draw();
 }
 
 void TitleScene::Finalize() {
 	delete sprite_;
 	delete sphere_;
 	delete block_;
+	delete upsidetank_;
+	delete downsidetank_;
+	delete debugcamera_;
 }
--- Blossom Engine/GameScenes/TitleScene.h
+++ Blossom Engine/GameScenes/TitleScene.h
@@ -3,8 +3,10 @@
 #include "../Blossom Engine/Sprite/Sprite.h"
 #include "../Blossom Engine/Sphere/Sphere.h"
 #include "../Blossom Engine/components/Input/Input.h"
+#include "../Necessary/Models/UpsideTank.h"
+#include "../Necessary/Models/DownsideTank.h"
 #include "../Necessary/Models/Block.h"
-
+#include "../Blossom Engine/components/camera/DebugCamera.h"
 class GameManager;
 
 class TitleScene : public IScene
@@ -15,10 +17,13 @@ public:
 	void Draw() override;
 	void Finalize()override;
 private:
+	DebugCamera* debugcamera_;
 	Sprite* sprite_;
 	Sphere* sphere_;
 	int textureNum_;
 	Input* input_;
 	Vector3 pos_;
 	Block* block_;
+	DownsideTank* downsidetank_;
+	UpsideTank* upsidetank_;
 };
