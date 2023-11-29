90d4be5eeb36fe68d17785391970adb6e6dd074c
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-11-30 02:11:39 +0900

    png追加

--- Blossom Engine/GameScenes/TitleScene.cpp
+++ Blossom Engine/GameScenes/TitleScene.cpp
@@ -14,7 +14,7 @@ void TitleScene::Initialize() {
 	downsidetank_ = new DownsideTank();
 	downsidetank_->Initialize();
 	downsidetank_->textureNum = DOWNSIDETANK;
-	pos_ = { 0,0,200 };
+	pos_ = { 0,0,2000};
 }
 
 void TitleScene::Update() {
