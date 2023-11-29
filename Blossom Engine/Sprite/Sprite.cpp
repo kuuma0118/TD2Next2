01fd32d8e462847272d519dd30aad64f3f3bfdfb
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-11-30 02:11:39 +0900

    png追加

--- Blossom Engine/Sprite/Sprite.cpp
+++ Blossom Engine/Sprite/Sprite.cpp
@@ -32,16 +32,16 @@ void Sprite::Initialize() {
 	};
 
 	// 矩形のデータ
-	vertexData_[0].position = { 0.0f, 360.0f, 0.0f, 1.0f };// 左下
+	vertexData_[0].position = { 0.0f, 720.0f, 0.0f, 1.0f };// 左下
 	vertexData_[0].texcoord = { 0.0f,1.0f };
 	vertexData_[0].normal = { 0.0f,0.0f,-1.0f };
 	vertexData_[1].position = { 0.0f, 0.0f, 0.0f, 1.0f };// 左上
 	vertexData_[1].texcoord = { 0.0f,0.0f };
 	vertexData_[1].normal = { 0.0f,0.0f,-1.0f };
-	vertexData_[2].position = { 640.0f, 360.0f, 0.0f, 1.0f };// 右下
+	vertexData_[2].position = { 1280.0f, 720.0f, 0.0f, 1.0f };// 右下
 	vertexData_[2].texcoord = { 1.0f,1.0f };
 	vertexData_[2].normal = { 0.0f,0.0f,-1.0f };
-	vertexData_[3].position = { 640.0f, 0.0f, 0.0f, 1.0f };// 右上
+	vertexData_[3].position = { 1280.0f, 0.0f, 0.0f, 1.0f };// 右上
 	vertexData_[3].texcoord = { 1.0f,0.0f };
 	vertexData_[3].normal = { 0.0f,0.0f,-1.0f };
 
